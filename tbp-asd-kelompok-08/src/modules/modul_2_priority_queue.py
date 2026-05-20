4b4953f5a566c2e5c8cb9df1209f1c955414013b
Amanda Lulu Prasetia <amandalulu.2025@student.uny.ac.id>
2026-05-20 23:33:23 +0700

    modul_2_priority_queue.py
    Refactor PriorityQueueKendaraan and Kendaraan classes, and remove old test functions.

--- tbp-asd-kelompok-08/src/modules/modul_2_priority_queue.py
+++ tbp-asd-kelompok-08/src/modules/modul_2_priority_queue.py
@@ -1,36 +1,46 @@
-import pytest
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from dataclasses import dataclass
 
-from src.modul_2_priority_queue import PriorityQueueKendaraan, Kendaraan
+# Node untuk linked list (digunakan juga oleh stack)
+class LLNode:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
-def test_enqueue_priority():
-    pq = PriorityQueueKendaraan()
-    motor = Kendaraan(1, 4, "P01", "-", 0)
-    ambulance = Kendaraan(2, 1, "P01", "-", 0)
-    bus = Kendaraan(3, 2, "P01", "-", 0)
-    pq.enqueue(motor)
-    pq.enqueue(ambulance)
-    pq.enqueue(bus)
-    # urutan keluar: ambulance (1), bus (2), motor (4)
-    assert pq.dequeue().jenis == 1
-    assert pq.dequeue().jenis == 2
-    assert pq.dequeue().jenis == 4
+@dataclass
+class Kendaraan:
+    id_kendaraan: int
+    jenis: int   # 1=AMBULANS (prioritas tertinggi), 2=BUS, 3=MOBIL, 4=MOTOR
+    asal: str
+    tujuan: str
+    waktu_masuk: float
 
-def test_fifo_same_priority():
-    pq = PriorityQueueKendaraan()
-    k1 = Kendaraan(1, 3, "", "", 0)
-    k2 = Kendaraan(2, 3, "", "", 0)
-    pq.enqueue(k1)
-    pq.enqueue(k2)
-    assert pq.dequeue().id_kendaraan == 1
-    assert pq.dequeue().id_kendaraan == 2
+class PriorityQueueKendaraan:
+    def __init__(self):
+        self.head = None
+        self._size = 0
 
-def test_len():
-    pq = PriorityQueueKendaraan()
-    assert len(pq) == 0
-    pq.enqueue(Kendaraan(1, 1, "", "", 0))
-    assert len(pq) == 1
-    pq.dequeue()
-    assert len(pq) == 0
+    def enqueue(self, kendaraan):
+        """O(n) insertion terurut berdasarkan prioritas (jenis kecil = prioritas tinggi)"""
+        node = LLNode(kendaraan)
+        if not self.head or kendaraan.jenis < self.head.data.jenis:
+            node.next = self.head
+            self.head = node
+        else:
+            curr = self.head
+            while curr.next and curr.next.data.jenis <= kendaraan.jenis:
+                curr = curr.next
+            node.next = curr.next
+            curr.next = node
+        self._size += 1
+
+    def dequeue(self):
+        """O(1)"""
+        if not self.head:
+            return None
+        val = self.head.data
+        self.head = self.head.next
+        self._size -= 1
+        return val
+
+    def __len__(self):
+        return self._size
