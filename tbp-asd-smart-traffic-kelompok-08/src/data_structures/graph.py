6a755f9126c16624254d67dabc1d2aea8e0ab857
alwanfadhilah2025-design <alwanfadhilah.2025@student.uny.ac.id>
2026-05-25 06:31:02 +0700

    Refactor EdgeNode and GraphJalan methods

--- tbp-asd-smart-traffic-kelompok-08/src/data_structures/graph.py
+++ tbp-asd-smart-traffic-kelompok-08/src/data_structures/graph.py
@@ -1,47 +1,83 @@
 class EdgeNode:
-    def __init__(self, dest, jarak, kapasitas_lajur):
+
+    def __init__(self, dest, jarak, lajur):
         self.dest = dest
         self.jarak = jarak
-        self.kapasitas_lajur = kapasitas_lajur
+        self.lajur = lajur
         self.next = None
 
+
 class GraphJalan:
+
     def __init__(self):
-        self.adj = {}          # adjacency list: node_name -> EdgeNode (linked list)
-        self.persimpangan = {} # menyimpan objek Persimpangan
+        self.adj = {}
+        self.persimpangan = {}
 
     def tambah_persimpangan(self, p):
+
         self.persimpangan[p.nama] = p
         self.adj[p.nama] = None
 
-    def tambah_jalan(self, asal, tujuan, jarak, lajur, dua_arah=True):
-        """O(1) tambah edge dari asal ke tujuan"""
-        node = EdgeNode(tujuan, jarak, lajur)
+    def tambah_jalan(
+        self,
+        asal,
+        tujuan,
+        jarak,
+        lajur,
+        dua_arah=True
+    ):
+
+        node = EdgeNode(
+            tujuan,
+            jarak,
+            lajur
+        )
+
         node.next = self.adj[asal]
         self.adj[asal] = node
 
         if dua_arah:
-            node2 = EdgeNode(asal, jarak, lajur)
+
+            node2 = EdgeNode(
+                asal,
+                jarak,
+                lajur
+            )
+
             node2.next = self.adj[tujuan]
             self.adj[tujuan] = node2
 
     def tetangga(self, nama):
-        """O(deg) mengembalikan list (dest, jarak)"""
+
         hasil = []
+
         curr = self.adj[nama]
+
         while curr:
-            hasil.append((curr.dest, curr.jarak))
+            hasil.append(
+                (curr.dest, curr.jarak)
+            )
             curr = curr.next
+
         return hasil
 
-    def degree(self, nama):
-        """Jumlah edge keluar dari simpul nama"""
-        count = 0
-        curr = self.adj[nama]
-        while curr:
-            count += 1
-            curr = curr.next
-        return count
+    def dfs(self, start):
 
-    def semua_simpul(self):
-        return list(self.adj.keys())
+        visited = set()
+
+        def _dfs(v):
+
+            visited.add(v)
+
+            curr = self.adj[v]
+
+            while curr:
+
+                if curr.dest not in visited:
+                    _dfs(curr.dest)
+
+                curr = curr.next
+
+        _dfs(start)
+
+        return visited
