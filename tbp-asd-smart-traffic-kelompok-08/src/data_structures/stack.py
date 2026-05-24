881d8529823ecc47352386fcd27bd79624163991
alwanfadhilah2025-design <alwanfadhilah.2025@student.uny.ac.id>
2026-05-25 06:28:28 +0700

    Implement Stack class with push, pop, and peek methods

--- tbp-asd-smart-traffic-kelompok-08/src/data_structures/stack.py
+++ tbp-asd-smart-traffic-kelompok-08/src/data_structures/stack.py
@@ -1,1 +1,30 @@
+from data_structures.linked_list import LLNode
 
+class Stack:
+
+    def __init__(self):
+        self.top = None
+        self._size = 0
+
+    def push(self, data):
+
+        node = LLNode(data)
+        node.next = self.top
+        self.top = node
+
+        self._size += 1
+
+    def pop(self):
+
+        if self.top is None:
+            return None
+
+        data = self.top.data
+        self.top = self.top.next
+
+        self._size -= 1
+
+        return data
+
+    def peek(self):
+        return self.top.data if self.top else None
