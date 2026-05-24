6fb3db0578bcc592383705c2001ba35bca1ad7f4
FaqihRizSap <faqihrizky.2025@student.uny.ac.id>
2026-05-25 03:04:01 +0700

    Refactor sorting functions for better clarity

--- tbp-asd-smart-traffic-kelompok-08/src/modules/modul_5_sorting.py
+++ tbp-asd-smart-traffic-kelompok-08/src/modules/modul_5_sorting.py
@@ -1,24 +1,43 @@
-def selection_sort_by_queue_length(queue_data):
-    """Selection sort pada list of tuple (nama, panjang_antrian) -> O(n^2)
-       Mengurutkan dari antrian terkecil ke terbesar."""
-    data = queue_data[:]
-    n = len(data)
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1, n):
-            if data[j][1] < data[min_idx][1]:
-                min_idx = j
-        data[i], data[min_idx] = data[min_idx], data[i]
-    return data
+def selection_sort(data):
 
-def insertion_sort_by_queue_length(queue_data):
-    """Insertion sort pada list of tuple -> O(n^2)"""
-    data = queue_data[:]
-    for i in range(1, len(data)):
-        key = data[i]
-        j = i-1
-        while j >= 0 and data[j][1] > key[1]:
-            data[j+1] = data[j]
+    arr = data[:]
+
+    n = len(arr)
+
+    for i in range(n):
+
+        idx = i
+
+        for j in range(i + 1, n):
+
+            if arr[j][1] > arr[idx][1]:
+                idx = j
+
+        arr[i], arr[idx] = (
+            arr[idx],
+            arr[i]
+        )
+
+    return arr
+
+
+def insertion_sort(data):
+
+    arr = data[:]
+
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+
+        j = i - 1
+
+        while (
+            j >= 0
+            and arr[j][1] < key[1]
+        ):
+            arr[j + 1] = arr[j]
             j -= 1
-        data[j+1] = key
-    return data
+
+        arr[j + 1] = key
+
+    return arr
