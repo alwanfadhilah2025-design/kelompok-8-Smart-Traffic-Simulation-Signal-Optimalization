9a933c06dd3b85db864d51256701947a927aabc9
Amanda Lulu Prasetia <amandalulu.2025@student.uny.ac.id>
2026-05-25 22:09:03 +0700

    modul_1_graph.py

--- tbp-asd-smart-traffic-kelompok-08/src/modules/modul_1_graph.py
+++ tbp-asd-smart-traffic-kelompok-08/src/modules/modul_1_graph.py
@@ -1,54 +1,13 @@
-@dataclass
-class Persimpangan:
-    nama: str
-    kapasitas: int = 20
+from data_structures.graph import GraphJalan
 
-class EdgeNode:
-    def __init__(self, dest, jarak, kapasitas_lajur):
-        self.dest = dest
-        self.jarak = jarak
-        self.kapasitas_lajur = kapasitas_lajur
-        self.next = None
+def cek_isolasi(graph):
 
-class GraphJalan:
-    def __init__(self):
-        self.adj = {}
-        self.persimpangan = {}
+    start = next(iter(graph.adj))
 
-    def tambah_persimpangan(self, p):
-        self.persimpangan[p.nama] = p
-        self.adj[p.nama] = None
+    visited = graph.dfs(start)
 
-    def tambah_jalan(self, asal, tujuan, jarak, lajur, dua_arah=True):
-        node = EdgeNode(tujuan, jarak, lajur)
-        node.next = self.adj[asal]
-        self.adj[asal] = node
-        if dua_arah:
-            node2 = EdgeNode(asal, jarak, lajur)
-            node2.next = self.adj[tujuan]
-            self.adj[tujuan] = node2
-
-    def tetangga(self, nama):
-        hasil = []
-        curr = self.adj[nama]
-        while curr:
-            hasil.append((curr.dest, curr.jarak))
-            curr = curr.next
-        return hasil
-
-def generate_jaringan(n=25, seed=17):
-    rng = np.random.default_rng(seed)
-    nama_p = [f'P{i:02d}' for i in range(n)]
-    persimpangan = [Persimpangan(nm, int(rng.integers(15, 30))) for nm in nama_p]
-    perm = rng.permutation(n)
-    edges = []
-    for i in range(1, n):
-        u = nama_p[perm[i-1]]
-        v = nama_p[perm[i]]
-        edges.append((u, v, int(rng.integers(100, 2000)), int(rng.integers(1, 4))))
-    for _ in range(15):
-        i, j = rng.choice(n, 2, replace=False)
-        edges.append((nama_p[i], nama_p[j],
-                      int(rng.integers(100,2000)),
-                      int(rng.integers(1,4))))
-    return persimpangan, edges
+    return [
+        v
+        for v in graph.adj
+        if v not in visited
+    ]
