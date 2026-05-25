eb14b42312edf2d3a60c932565926ed4a5c60817
alwanfadhilah2025-design <alwanfadhilah.2025@student.uny.ac.id>
2026-05-25 07:38:26 +0700

    Implement test for BSTJalan insertion and search
    Add test for BSTJalan class with insert and search methods.

--- tbp-asd-smart-traffic-kelompok-08/tests/test_4_bst.py
+++ tbp-asd-smart-traffic-kelompok-08/tests/test_4_bst.py
@@ -1,1 +1,20 @@
+from data_structures.bst import BSTJalan
 
+class P:
+
+    def __init__(self,nama):
+        self.nama = nama
+
+bst = BSTJalan()
+
+bst.insert("P03",P("P03"))
+bst.insert("P01",P("P01"))
+bst.insert("P02",P("P02"))
+
+hasil = bst.search("P02")
+
+print("Ditemukan:",hasil.nama)
+
+assert hasil.nama == "P02"
+
+print("TEST BST BERHASIL")
