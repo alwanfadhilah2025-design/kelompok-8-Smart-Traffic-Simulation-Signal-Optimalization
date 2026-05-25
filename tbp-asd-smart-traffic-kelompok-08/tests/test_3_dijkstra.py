7a6c9d2d8d637d44a2056f61b8dc75eff126f1b3
alwanfadhilah2025-design <alwanfadhilah.2025@student.uny.ac.id>
2026-05-25 07:37:25 +0700

    Add Dijkstra test case for route and distance

--- tbp-asd-smart-traffic-kelompok-08/tests/test_3_dijkstra.py
+++ tbp-asd-smart-traffic-kelompok-08/tests/test_3_dijkstra.py
@@ -1,1 +1,31 @@
+from modules.modul3_dijkstra import (
+    dijkstra_rute,
+    ambil_jalur
+)
 
+from data_structures.graph import GraphJalan
+
+class P:
+
+    def __init__(self, nama):
+        self.nama = nama
+
+g = GraphJalan()
+
+g.tambah_persimpangan(P("P01"))
+g.tambah_persimpangan(P("P02"))
+g.tambah_persimpangan(P("P03"))
+
+g.tambah_jalan("P01","P02",100,2)
+g.tambah_jalan("P02","P03",150,2)
+
+dist,parent = dijkstra_rute(g,"P01")
+
+path = ambil_jalur(parent,"P03")
+
+print("Rute :", path)
+print("Jarak:", dist["P03"])
+
+assert dist["P03"] == 250
+
+print("TEST DIJKSTRA BERHASIL")
