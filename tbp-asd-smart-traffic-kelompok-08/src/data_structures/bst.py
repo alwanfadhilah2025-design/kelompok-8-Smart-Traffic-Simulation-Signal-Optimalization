05a52c53ee2e338768383a455054203114ef78d9
arwanadrean2025-lab <arwanadrean.2025@student.uny.ac.id>
2026-05-25 14:20:31 +0700

    Update bst.py

--- tbp-asd-smart-traffic-kelompok-08/src/data_structures/bst.py
+++ tbp-asd-smart-traffic-kelompok-08/src/data_structures/bst.py
@@ -1,45 +1,65 @@
 class BSTNode:
+
     def __init__(self, nama, p):
-        self.nama = nama   # key (nama persimpangan)
-        self.p = p         # data (objek Persimpangan)
+
+        self.nama = nama
+        self.p = p
+
         self.left = None
         self.right = None
 
+
 class BSTJalan:
+
     def __init__(self):
         self.root = None
 
     def insert(self, nama, p):
-        """O(log n) rata-rata, O(n) worst-case"""
-        def _insert(root, nama, p):
+
+        def _insert(root):
+
             if root is None:
                 return BSTNode(nama, p)
+
             if nama < root.nama:
-                root.left = _insert(root.left, nama, p)
+                root.left = _insert(root.left)
             else:
-                root.right = _insert(root.right, nama, p)
+                root.right = _insert(root.right)
+
             return root
-        self.root = _insert(self.root, nama, p)
+
+        self.root = _insert(self.root)
 
     def search(self, nama):
-        """O(log n) rata-rata"""
+
         curr = self.root
+
         while curr:
-            if nama == curr.nama:
+
+            if curr.nama == nama:
                 return curr.p
-            elif nama < curr.nama:
+
+            if nama < curr.nama:
                 curr = curr.left
             else:
                 curr = curr.right
+
         return None
 
     def inorder(self):
-        """Mengembalikan daftar nama persimpangan terurut"""
-        res = []
-        def _inorder(node):
+
+        hasil = []
+
+        def _in(node):
+
             if node:
-                _inorder(node.left)
-                res.append(node.nama)
-                _inorder(node.right)
-        _inorder(self.root)
-        return res
+
+                _in(node.left)
+
+                hasil.append(node.nama)
+
+                _in(node.right)
+
+        _in(self.root)
+
+        return hasil
